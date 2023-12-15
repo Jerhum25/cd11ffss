b6695c9a0f4e4c3d95a05a176531445aadadec71
jerhum <jerhum@msn.com>
2023-12-15 14:28:00 +0100

    carrousel responsive

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from "react";
-// import "./slider.scss";
-import s from './style.module.scss'
+import s from "./style.module.scss";
 
-const Slider = () => {
+const Carrousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   // Images pour le slider
   const images = [
-    { url: "./img/carrousel1.jpg", title: "Protéger", description:"Nous protégeons et sécurisons vos manifestations événementielles" },
-    { url: "./img/carrousel2.png", title: "Secourir", description:"Nous sommes au service de la population en cas de sinistre" },
-    { url: "./img/carrousel3.png", title: "Former", description:"Nous vous formons pour sauver des vies" },
+    {
+      url: "./img/carrousel1.jpg",
+      title: "Protéger",
+      description:
+        "Nous protégeons et sécurisons vos manifestations événementielles",
+    },
+    {
+      url: "./img/carrousel2.png",
+      title: "Secourir",
+      description: "Nous sommes au service de la population en cas de sinistre",
+    },
+    {
+      url: "./img/carrousel3.png",
+      title: "Former",
+      description: "Nous vous formons à sauver des vies",
+    },
   ];
   // Fonction pour passer au slide suivant
   const nextSlide = () => {
@@ -25,14 +37,14 @@ const Slider = () => {
     }, 5000); // Changez ici le délai entre chaque défilement en millisecondes (par exemple, 5000 pour 5 secondes)
 
     return () => clearInterval(interval);
-  }, []);
+  },[]);
 
   return (
-    <div className={s.slider}>
+    <div className={s.container}>
       {images.map((image, index) => (
         <div
           key={index}
-          className={index === currentSlide ? [s.active] : s.slide}
+          className={index === currentSlide ? s.active : s.slide}
           style={{ backgroundImage: `url(${image.url})` }}
         >
           <h2 className={s.title}>{image.title}</h2>
@@ -43,4 +55,4 @@ const Slider = () => {
   );
 };
 
-export default Slider;
+export default Carrousel;
